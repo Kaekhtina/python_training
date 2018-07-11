d0f88c2dff7ec4b813e805a30496d70eca25aa0f
okaekhtina <okaekhtina@ozon.ru>
2018-07-11 16:40:17 +0300

    Задание 4(поправила application)

--- application.py
+++ application.py
@@ -6,14 +6,20 @@ class Application:
         self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
 
+
     def logout(self):
         wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
 
-    def create_group(self, group):
+    def open_groups_page(self):
         wd = self.wd
         wd.find_element_by_link_text("groups").click()
+
+
+    def create_group(self, group):
+        wd = self.wd
+        self.open_groups_page()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -25,6 +31,11 @@ class Application:
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+
+    def return_to_groups_page(self):
+        wd = self.wd
         wd.find_element_by_link_text("groups").click()
 
 
@@ -38,6 +49,7 @@ class Application:
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
 
     def destroy(self):
         self.wd.quit()

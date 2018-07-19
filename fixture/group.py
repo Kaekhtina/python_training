22182c72d5c90db6d4e9ba453a3b54bb085d627d
okaekhtina <okaekhtina@ozon.ru>
2018-07-19 14:12:03 +0300

    Задание 10 (оптимизация переходов)

--- fixture/group.py
+++ fixture/group.py
@@ -8,6 +8,8 @@ class GroupHelper:
 
     def open_groups_page(self):
         wd = self.app.wd
+        if wd.carrent_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0:
+            return
         wd.find_element_by_link_text("groups").click()
 
 
